c4533bbc479b3c5ef0d853e6c5a8a088aa877aed
josepmdc <jdomingocatafal@gmail.com>
2020-12-15 16:25:52 +0100

    Day 14 part 1

--- 14th/main.go
+++ 14th/main.go
@@ -8,43 +8,31 @@ import (
 	"strings"
 )
 
-type modifier struct {
-	pos int
-	val rune
-}
-
 func main() {
-	data := util.GetData("14th/input_test")
+	data := util.GetData("14th/input")
 	part1(data)
 }
 
 func part1(data []string) {
-	var mask string
-	var modifiers []modifier
-	memory := make(map[int]int)
+	var andMask int64
+	var orMask int64
+	memory := make(map[int]int64)
 	for _, inst := range data {
 		if strings.Contains(inst, "mask") {
-			mask = strings.Split(data[0], " = ")[1]
-			for i, char := range mask {
-				if char != 'X' {
-					modifiers = append(modifiers, modifier{pos: i, val: char})
-				}
-			}
+			mask := strings.Split(inst, " = ")[1]
+			andMask, _ = strconv.ParseInt(strings.ReplaceAll(mask, "X", "1"), 2, 64)
+			orMask, _ = strconv.ParseInt(strings.ReplaceAll(mask, "X", "0"), 2, 64)
 		} else {
 			r := regexp.MustCompile(`^mem\[(.*?)\] = ([0-9]+)$`).FindStringSubmatch(inst)
 			pos, _ := strconv.Atoi(r[1])
 			v, _ := strconv.Atoi(r[2])
-			val := []rune("00000000000000000000000000000000000000000000000000000000" +
-				strconv.FormatInt(int64(v), 2))
-			val = val[len(val)-len(mask):]
-			for _, m := range modifiers {
-				val[m.pos] = m.val
-			}
-			tmp, _ := strconv.ParseInt(string(val), 2, 64)
-			memory[pos] = int(tmp)
+			val := int64(v)
+			val &= andMask
+			val |= orMask
+			memory[pos] = val
 		}
 	}
-	sum := 0
+	var sum int64
 	for _, v := range memory {
 		sum += v
 	}
