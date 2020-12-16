6b2e8bd0b434671677d3f5ed8c602eafaacba46b
josepmdc <jdomingocatafal@gmail.com>
2020-12-16 17:44:30 +0100

    Rename folders

--- 14/main.go
+++ 14/main.go
@@ -38,3 +38,34 @@ func part1(data []string) {
 	}
 	fmt.Printf("Part 1: %d\n", sum)
 }
+
+func part2(data []string) {
+	var andMask int64
+	var orMask int64
+	memory := make(map[int]int64)
+	var mask string
+	var x int
+	for _, inst := range data {
+		if strings.Contains(inst, "mask") {
+			mask = strings.Split(inst, " = ")[1]
+			x = strings.Count(mask, "X")
+			andMask, _ = strconv.ParseInt(strings.ReplaceAll(mask, "X", "1"), 2, 64)
+			orMask, _ = strconv.ParseInt(strings.ReplaceAll(mask, "X", "0"), 2, 64)
+		} else {
+			r := regexp.MustCompile(`^mem\[(.*?)\] = ([0-9]+)$`).FindStringSubmatch(inst)
+			pos, _ := strconv.Atoi(r[1])
+			v, _ := strconv.Atoi(r[2])
+			val := int64(v)
+			for i := 0; i < 2^x; i++ {
+			}
+			val &= andMask
+			val |= orMask
+			memory[pos] = val
+		}
+	}
+	var sum int64
+	for _, v := range memory {
+		sum += v
+	}
+	fmt.Printf("Part 1: %d\n", sum)
+}
